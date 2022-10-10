b628905df4c32e47047314113e6ef42da91dd90b
ivanminutillo <ivanminutillo@outlook.it>
2022-10-10 18:25:02 +0200

    bonfire

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,17 +17,8 @@ module.exports = {
     themes: [
       {
         bonfire: {
-          "primary": "#fde047", 
-          "secondary": "#fb923c",
-          "accent": "#1FB2A6",
-          "neutral": "#191D24",
-          "base-100": "#1E293B",
-          "base-300": "#0F172A",
-          "base-content": "#E2E8F0",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#f15b5b",
+          ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
+          "primary": "#fde047"
         },
       },
     ]
