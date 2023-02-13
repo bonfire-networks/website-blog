e28f5ebd5e17b2702d060d7605e7ea499df04164
ivanminutillo <ivanminutillo@outlook.it>
2023-02-13 16:56:39 +0100

    up

--- _data/release.js
+++ _data/release.js
@@ -1,14 +1,14 @@
 const { Octokit } = require("@octokit/core");
 const octokit = new Octokit({auth: process.env.GITHUB_TOKEN});
 
-
 module.exports = async function() {
-  return octokit.request('GET https://raw.githubusercontent.com/{owner}/{repo}/main/docs/{path}', {
+  return octokit.request('GET /repos/{owner}/{repo}/releases', {
     owner: 'bonfire-networks',
     repo: 'bonfire-app',
-    path: 'BONFIRE-FLAVOURED-ELIXIR.md'
-  }).then(res => { 
-    return res.data
-    })
+  })
+  .then(res => { 
+    console.log(res.data[0].tag_name)
+    return res.data[0].tag_name
+  })
   .catch(err => { console.log(err) })
 };
