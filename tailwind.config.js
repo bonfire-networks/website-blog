8c67947139bb55a748f4ab4cf88b8ee8c22c5c1d
ivanminutillo <ivanminutillo@outlook.it>
2023-07-09 18:08:55 +0200

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,17 +22,17 @@ module.exports = {
           ...require("daisyui/src/theming/themes")["[data-theme=light]"],
           "primary": "#1B74E4",
           "primary-content": "#fff",
-          "base-300": "#fff",
+          "base-300": "#131415",
           "base-200": "#F7F9F9",
-          "base-100": "#F0F2F5"
+          "base-100": "#1E2023"
         },
         bonfire: {
           ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
           "primary": "#fde047",
           "primary-content": "#112A46",
           "base-content": "#e8e8e8",
-          "secondary": "#414558",
-          "secondary-content": "#C2CBF5"
+          "secondary": "#A7B5FF",
+          "secondary-content": "#131415"
         }
       },
     ]
