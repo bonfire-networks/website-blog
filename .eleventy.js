520dd8a833fd9a4f07949d2b428738ff4134844c
Mayel de Borniol <git@mayel.space>
2026-05-20 16:42:59 +0100

    add json client

--- .eleventy.js
+++ .eleventy.js
@@ -158,6 +158,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("archives");
   eleventyConfig.addPassthroughCopy("fediforum");
+  eleventyConfig.addPassthroughCopy(".well-known");
 
   // Ignore CSS output file to prevent watch loop
   eleventyConfig.watchIgnores.add("css/style_v2.css");
